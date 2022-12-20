3c519a2ba73238f6e9112a5abaf8d1bb05a0e070
darshil152 <darshillunagariya07@gmail.com>
2022-12-20 17:34:56 +0530

    cart changes

--- src/cart.js
+++ src/cart.js
@@ -1,26 +1,12 @@
 import { CardTravelSharp, CloseFullscreen, ImportExport } from '@mui/icons-material'
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Table from 'react-bootstrap/Table';
 
 
 export default function Cart() {
 
-  const items = localStorage.getItem('cartitems') ? JSON.parse(localStorage.getItem('cartitems')) : [];
-  const items1 = localStorage.getItem('productdetail') ? JSON.parse(localStorage.getItem('productdetail')) : [];
-  const [data, setData] = useState([]);
+  const [items, setItems] = useState(localStorage.getItem('cartitems') ? JSON.parse(localStorage.getItem('cartitems')) : []);
   const [counter, setCounter] = useState(1);
-
-
-
-  useEffect(() => {
-    console.log(items1)
-    for (let i = 0; i < items.length; i++) {
-      if (items[i] == items1[i].id) {
-        setData(items1[i])
-        console.log(data)
-      }
-    }
-  }, [])
 
 
   const decreaseQty = (data) => {
@@ -37,49 +23,23 @@ export default function Cart() {
   };
 
 
-
-  useEffect(() => {
-    console.log(old);
-    for (let i = 0; i < old.length; i++) {
-      if (old[i].id == newdata[i]) {
-        setCurrentdata(old[i])
-      }
+  const decrease = (data) => {
+    if (counter > 0) {
+      setCounter(count => count - 1);
+      const totalPrice2 = data * (counter - 1)
+      console.log(totalPrice2);
     }
-  }, [])
-  
-  console.log(currentdata)
-
-  // const decreaseQty = (data) => {
-  //   console.log(data);
-  // }
-
-  // //-----------------------increment decrement --------------------------//
-
-  // const increase = (data) => {
-  //   setCounter(count => count + 1);
-
-  //   const totalPrice = data * (counter + 1)
-  //   console.log(totalPrice);
-  // };
+  };
+  const reset = (data) => {
+    setCounter(1)
+    const totalPrice3 = data * counter
+    console.log(totalPrice3);
+  }
 
 
-  // const decrease = (data) => {
-  //   if (counter > 0) {
-  //     setCounter(count => count - 1);
-  //     const totalPrice2 = data * (counter - 1)
-  //     console.log(totalPrice2);
-  //   }
-  // };
-  // const reset = (data) => {
-  //   setCounter(1)
-  //   const totalPrice3 = data * counter
-  //   console.log(totalPrice3);
-  // }
-
-
-  // const sendprice = (data) => {
-  //   console.log("Total Price", data * items.price)
-  // }
+  const sendprice = (data) => {
+    console.log("Total Price", data * items.price)
+  }
 
 
   // const clear = (data) =>{
@@ -94,9 +54,9 @@ export default function Cart() {
 
   return (
     <>
-      {/* <div>
+      <div>
         {
-          data.length > 0 && data.map((datas) => {
+          items.length > 0 && items.map((datas) => {
             return (
               <div className='container'>
                 <div className='row'>
@@ -120,10 +80,7 @@ export default function Cart() {
             )
           })
         }
-      </div> */}
-
-
-
+      </div>
     </>
   )
 }
